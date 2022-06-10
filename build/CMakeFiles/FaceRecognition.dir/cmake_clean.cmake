0c82000e814d15527c8e2fa66c0246d07df64eeb
Ignacio <rogerpeke97@gmail.com>
2022-06-09 22:29:44 -0300

    setup dependencies, sdl2 as submodule and imgui files for the interface

---
+++ build/CMakeFiles/FaceRecognition.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui_impl_sdl.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/FaceRecognition.dir/src/main.cpp.o"
+  "FaceRecognition"
+  "FaceRecognition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceRecognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ configure.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
